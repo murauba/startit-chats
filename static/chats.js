335203242a275825f51b5f8ee585b505d3b7af92
murauba <54527385+murauba@users.noreply.github.com>
2020-01-10 15:17:25 +0200

    0,3

--- static/chats.js
+++ static/chats.js
@@ -1,1 +1,18 @@
 console.log("konsole");
+const refr=5000
+
+async function lasit(){
+    const ret = await fetch('chat/lasi');
+    const dataObject=await ret.json();
+    showsimple(dataObject);
+}
+
+function showsimple(dati){
+    const newline="</br>";
+    let chats = "";
+    let chatadiv=document.getElementById("chats");
+    for (let rinda of dari['chats']){
+        chats=chats+rinda+newline;
+    }
+    chatadiv.innerHTML=chats;
+}
