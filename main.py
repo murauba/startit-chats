5136759d3581bb55cac0e4d21079c56a82f2bbf7
murauba <54527385+murauba@users.noreply.github.com>
2020-01-10 14:04:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,15 @@ def index_page():
 def health_check():
   return "ok"
 
+
+@app.route('/chat/lasis')
+def ielasit_chatu():
+  chata_rindas=[]
+  with open("chats.txt","r") as f:
+    for rinda in f:
+      chata_rindas.append(rinda)
+  return jsonify({"chats:"chata_rindas})
+
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000, debug=True)
